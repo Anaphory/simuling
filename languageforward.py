77244536b650e975ccee75092bab0a26e9430a16
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2016-09-17 23:16:45 +0200

    Write output do Data/ subdirectory
    This is sumatras default output directory.

--- languageforward.py
+++ languageforward.py
@@ -6,4 +6,5 @@ import sys
 
 if __name__=="__main__":
     options = json.load(open(sys.argv[1]))
-    print(options)
+    with open("Data/output", "w") as output:
+        print(options, file=output)

1456132296e9f1571394485e9a2d36de27364bbb
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2016-09-18 10:10:00 +0200

    Test the CLI
    Add tests to see the CLI succeed or fail as expected.

--- test_languageforward.py
+++ test_languageforward.py
@@ -4,11 +4,20 @@ import pytest
 import networkx
 
 
-@pytest.fixture
-def lfs():
-    return LanguageForwardSimulation(networkx.krackhardt_kite_graph(), 0)
+@pytest.fixture(params=[networkx.krackhardt_kite_graph(),
+                        networkx.binomial_graph(8, 0.5, seed=0)])
+def lfs(request):
+    return LanguageForwardSimulation(request.param, 0)
 
 
 def test_speaker_is_set(lfs):
     assert lfs.population_graph.node[0]['speaker'], (
         "Node 0 should have a speaker attached")
+
+
+def test_run_lfs(lfs):
+    lfs.run(100)
+
+
+def test_time_step(lfs):
+    lfs.run(1)

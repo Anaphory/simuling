092491f602bdda65c269f32015eca8d5878699c1
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2016-09-18 09:22:36 +0200

    Add a speaker to every node and test that
    Add a Speaker object to each node in the social network.
    Test this, using two trivial tests for LFS initialization.

--- languageforward.py
+++ languageforward.py
@@ -18,10 +18,11 @@ class LanguageForwardSimulation (object):
     A LanguageForwardSimulation represents a forward-time stochastic
     simulation of an evolving population of speakers.
     """
-    def __init__(self, population_graph, seed):
-        self.population_graph = population_graph
+    def __init__(self, population_graph, seed=0):
+        self.population_graph = population_graph.copy()
         self.seed = seed
-        ...
+        for node in self.population_graph.nodes():
+            self.population_graph.node[node]["speaker"] = Speaker()
 
     def run(self, time_steps):
         for i in range(time_steps):
@@ -34,8 +35,8 @@ if __name__ == "__main__":
     """ randomizer seed used for the simulation """
 
     population_graph = options["population_graph"]
-    if population_graph == 'binomial':
-        population = networkx.binomial_graph(1000, 0.1, seed=seed)
+    if population_graph == 'krackhardt':
+        population = networkx.krackhardt_kite_graph()
     else:
         raise ValueError("Graph model {:s} not recognized.".format(
             population_graph))
@@ -43,5 +44,5 @@ if __name__ == "__main__":
     simulation = LanguageForwardSimulation(population, seed)
     simulation.run(options["time_steps"])
 
-    with open("Data/population", "w") as output:
+    with open("Data/population", 'w') as output:
         print(simulation.population_graph.edges(), file=output)
--- test_languageforward.py
+++ test_languageforward.py
@@ -1,2 +1,14 @@
-def empty_test():
-    pass
+from languageforward import *
+import pytest
+
+import networkx
+
+
+@pytest.fixture
+def lfs():
+    return LanguageForwardSimulation(networkx.krackhardt_kite_graph(), 0)
+
+
+def test_speaker_is_set(lfs):
+    assert lfs.population_graph.node[0]['speaker'], (
+        "Node 0 should have a speaker attached")

f38787cea91e4ae05c94015f334b93adebd88134
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2016-09-18 11:16:55 +0200

    Add the core learning loop

--- languageforward.py
+++ languageforward.py
@@ -3,13 +3,13 @@
 import networkx
 import json
 import sys
+import random
 
 
 class Speaker (object):
     """A single speaker agent in a LanguageForwardSimulation."""
     def __init__(self):
         ...
-    ...
 
 
 class LanguageForwardSimulation (object):
@@ -18,15 +18,31 @@ class LanguageForwardSimulation (object):
     A LanguageForwardSimulation represents a forward-time stochastic
     simulation of an evolving population of speakers.
     """
-    def __init__(self, population_graph, seed=0):
+    def __init__(self, population_graph, critical_period, seed=0):
         self.population_graph = population_graph.copy()
         self.seed = seed
         for node in self.population_graph.nodes():
             self.population_graph.node[node]["speaker"] = Speaker()
+        self.random = random.Random(self.seed)
+        self.critical_period = critical_period
+
+    def random_speaker(self):
+        return self.random.choice(self.population_graph.nodes())
+
+    def communication(self, speakers, learner):
+        # This is mostly a static method – we could in theory be more
+        # object-oriented and derive the speakers from knowing the
+        # learner, or be more static and also pass a randomizer. The
+        # current compromise is not optimal.
+        ...
 
     def run(self, time_steps):
         for i in range(time_steps):
-            ...
+            die = self.random_speaker()
+            self.population_graph.node[die]["speaker"] = child = Speaker()
+            for t in range(self.critical_period):
+                self.communication(self.population_graph[die], child)
+
 
 if __name__ == "__main__":
     options = json.load(open(sys.argv[1]))
@@ -41,7 +57,9 @@ if __name__ == "__main__":
         raise ValueError("Graph model {:s} not recognized.".format(
             population_graph))
 
-    simulation = LanguageForwardSimulation(population, seed)
+    t = options.get("critical_period", 1)
+
+    simulation = LanguageForwardSimulation(population, t, seed)
     simulation.run(options["time_steps"])
 
     with open("Data/population", 'w') as output:
--- test_languageforward.py
+++ test_languageforward.py
@@ -10,6 +10,11 @@ def lfs(request):
     return LanguageForwardSimulation(request.param, 0)
 
 
+@pytest.fixture
+def speaker():
+    return Speaker()
+
+
 def test_speaker_is_set(lfs):
     assert lfs.population_graph.node[0]['speaker'], (
         "Node 0 should have a speaker attached")
@@ -20,4 +25,28 @@ def test_run_lfs(lfs):
 
 
 def test_time_step(lfs):
+    before = networkx.Graph(lfs.population_graph)
     lfs.run(1)
+    after = lfs.population_graph
+    diff = 0
+    for node in before.nodes():
+        assert node in after.nodes()
+        if before.node[node]['speaker'] != after.node[node]['speaker']:
+            print(before.node[node]['speaker'], after.node[node]['speaker'])
+            diff += 1
+    assert diff == 1
+
+
+def test_random_speaker(lfs):
+    nodes = lfs.population_graph.nodes()
+    counter = {node: 0 for node in nodes}
+    k = 10000
+    for _ in range(len(nodes)*k):
+        counter[lfs.random_speaker()] += 1
+    for val in counter.values():
+        assert k*0.9 < val < k/0.9
+    # FIXME: There are cleaner ways to run stochastic tests, use one.
+
+
+def test_communication(lfs, speaker):
+    ...

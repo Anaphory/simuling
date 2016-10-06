8afcaf312c4006639d3a12b33e50eb6c2e0d82a1
Gereon Kaiping <anaphory@yahoo.de>
2016-10-06 16:49:53 +0200

    Make PEP8 compatible
    I don't actually mind that much, but given that I have configured travis
    to care, it now bothers me if something is not in PEP8 style.

--- phylo/phylo.py
+++ phylo/phylo.py
@@ -4,10 +4,11 @@ from itertools import combinations
 import lingpy
 import random
 
+
 class Language(object):
     """
     A language is modelled as a bipartite graph.
-    
+
     Notes
     -----
     Nothing special, we have words, concepts, and relations between the
@@ -32,15 +33,14 @@ class Language(object):
         * ww: parameter to handle the probability of cross-linking concept and
               word connections upon initialization
     """
-    def __init__(self, signs, fields, max_syns=2, 
-            params=None
-            ):
-        
+    def __init__(self,
+                 signs, fields, max_syns=2, params=None):
+
         self.params = params or dict(
-                ll = 2,
-                al = 2,
-                nw = 10,
-                ww = 10,
+                ll=2,
+                al=2,
+                nw=10,
+                ww=10,
                 )
         self.concepts = list(range(signs))
         self.words = list(range(signs))
@@ -54,7 +54,7 @@ class Language(object):
         self.signs = []
         self.concept2form = defaultdict(set)
         for i, j in combinations(self.concepts, r=2):
-            if not random.randint(0,1):
+            if not random.randint(0, 1):
                 if len(self.concept2form[i]) >= max_syns:
                     pass
                 else:
@@ -62,7 +62,7 @@ class Language(object):
                         self.signs += [(i, j)]
                     self.concept2form[i].add(j)
 
-        self.tracer = {0 : [(a,b) for a,b in self.signs]}
+        self.tracer = {0: [(a, b) for a, b in self.signs]}
 
     def _loose_link(self):
         """
@@ -73,16 +73,15 @@ class Language(object):
         Weights are modelled as recurring links in the list. This makes it
         easier (at least for me ...) to handle the selection process in a
         stochastic (?) manner (links which occur frequently are more likely to
-        be deleted). 
+        be deleted).
         """
 
         idx = random.randint(0, len(self.signs)-1)
         self.signs.pop(idx)
 
-
     def _add_link(self):
         """
-        Add a link between a concept and a word. 
+        Add a link between a concept and a word.
 
         Note
         ----
@@ -91,7 +90,7 @@ class Language(object):
         to, e.g., homophony), and then select the new 'concept' from this
         restricted set.
         """
-        
+
         widx = random.choice(self.words)
 
         # get the concepts
@@ -99,10 +98,12 @@ class Language(object):
 
         # get the semantic fields
         fields = [self.concept2field[c] for c in concepts]
-        
+
         if fields:
-            targets = [c for c in self.concepts if self.concept2field[c] in fields]
-        
+            targets = [c
+                       for c in self.concepts
+                       if self.concept2field[c] in fields]
+
             new_link = random.choice(targets)
             self.signs += [(new_link, widx)]
 
@@ -115,7 +116,7 @@ class Language(object):
         'words' is a list showing words that have been newly created in other
         languages so far, to avoid that this language creates the same ones.
         """
-        
+
         if words:
             new_word = max(words)+1
         else:
@@ -123,7 +124,7 @@ class Language(object):
         concept = random.choice(self.concepts)
         self.signs += [(concept, new_word)]
         self.words += [new_word]
-    
+
     def clone(self):
         """
         Make a copy of a language of itself. Needed for phylogenies.
@@ -134,11 +135,11 @@ class Language(object):
         tmp.words = [i for i in self.words]
         tmp.fields = [i for i in self.fields]
         tmp.concept2field = dict([(a, b) for a, b in
-            self.concept2field.items()])
-        tmp.signs = [(a, b) for a,b in self.signs]
+                                  self.concept2field.items()])
+        tmp.signs = [(a, b) for a, b in self.signs]
         tmp.tracer = {0: [(a, b) for a, b in tmp.signs]}
         return tmp
-    
+
     def change(self, time, words=[]):
         """
         Basic process, time is an integer.
@@ -167,20 +168,23 @@ class Language(object):
         for idx in basic:
             refs = comp.get(idx, [])
             refs_sorted = sorted(set(refs), key=lambda x: refs.count(x),
-                    reverse=True)
+                                 reverse=True)
             if refs:
                 best_refs = sum([refs.count(x) for x in refs_sorted[:3]])
-                selected = [x for x in refs_sorted[:3] if refs.count(x) > \
-                        best_refs / 3] or [refs_sorted[0]]
+                selected = [
+                    x
+                    for x in refs_sorted[:3]
+                    if refs.count(x) > best_refs / 3] or [refs_sorted[0]]
             else:
                 selected = []
             basics[idx] = selected
         return basics
 
+
 class Phylogeny(object):
     """
     The main simulation handler.
-    
+
     Notes
     -----
     Uses lingpy to get a random tree, uses also the lingpy.Tree class to handle
@@ -192,25 +196,26 @@ class Phylogeny(object):
 
     def __init__(self, signs, fields, max_syns=2, params=None):
 
-        self.language = Language(signs, fields, max_syns=max_syns, params=params)
+        self.language = Language(
+            signs, fields, max_syns=max_syns, params=params)
         params = params or self.language.params
-        if not 'change_range' in params:
+        if 'change_range' not in params:
             params['change_range'] = 1000
         self.params = params
 
     def prepare(self, taxa, change_range=None, logfile='phylo.log',
-            change_min=500):
+                change_min=500):
         if change_range:
             self.params['change_range'] = change_range
         self.params['change_min'] = change_min
-        self.tree = lingpy.basic.tree.Tree(lingpy.basic.tree.random_tree(taxa,
-            branch_lengths=False))
-    
+        self.tree = lingpy.basic.tree.Tree(lingpy.basic.tree.random_tree(
+            taxa, branch_lengths=False))
+
     def start(self, basic=None, verbose=True):
         self.basic = basic or list(range(100))
         self.words = self.language.words
         self.log = dict(
-                root = self.language.tracer[0]
+                root=self.language.tracer[0]
                 )
         self.tracer = {}
         self.siblings = {}
@@ -222,17 +227,23 @@ class Phylogeny(object):
                         distance=0
                         )
             else:
-                new_language = self.tracer[node.Parent.Name]['language'].clone()
-                distance = random.randint(self.params['change_min'], self.params['change_range'])
-                if verbose: print('... analyzing node {0} ({1})'.format(node.Name, distance))
+                new_language = self.tracer[
+                    node.Parent.Name]['language'].clone()
+                distance = random.randint(
+                    self.params['change_min'],
+                    self.params['change_range'])
+                if verbose:
+                    print('... analyzing node {0} ({1})'.format(
+                        node.Name, distance))
 
                 new_language.change(distance, words=self.words)
                 self.tracer[node.Name] = dict(
-                        language= new_language,
-                        tracer= new_language.tracer[1],
+                        language=new_language,
+                        tracer=new_language.tracer[1],
                         distance=distance)
-                self.words += [w for w in new_language.words if w not in
-                        self.words]
+                self.words += [w
+                               for w in new_language.words
+                               if w not in self.words]
                 if node.istip():
                     self.siblings[node.Name] = new_language.count(self.basic)
 
@@ -240,7 +251,7 @@ class Phylogeny(object):
         """
         Create a wordlist of the data (easy to use in lingpy).
         """
-        
+
         D = {}
         idx = 1
         for concept in self.basic:
@@ -253,7 +264,7 @@ class Phylogeny(object):
         return wl
 
 if __name__ == '__main__':
-    
+
     """
     Main idea is to run the simulation some time, calculate neighbor and upgma
     trees, and compare the results, using Robinson Foulds distance (or how this
@@ -267,22 +278,23 @@ if __name__ == '__main__':
     """
     dists1, dists2, dists3 = [], [], []
     for i in range(100):
-        #print('[i] analyzing setting {0}'.format(i+1))
+        # print('[i] analyzing setting {0}'.format(i+1))
         phy = Phylogeny(1000, 50)
 
         # add more letters to get more taxa
         taxa = list('abcdefghijklmnopqrst'.upper())
         # change range: maximal number of changes, change_min: minimal number
         # of changes (if it's not enough, nothing will change)
-        phy.prepare(taxa, change_range=2000,
-                change_min=1900)
+        phy.prepare(taxa,
+                    change_range=2000,
+                    change_min=1900)
         # "basic" is the number of words we afterwards use to to infer
         # phylogeny with neighbor-joining
         phy.start(basic=list(range(200)))
         wl = phy.wordlist()
 
-        
-        wl.add_entries('cog2', 'concept,cogid', lambda x, y: str(x[y[0]]) + '-' + str(x[y[1]]))
+        wl.add_entries('cog2', 'concept,cogid',
+                       lambda x, y: str(x[y[0]]) + '-' + str(x[y[1]]))
         wl.renumber('cog2')
         wl.calculate('diversity', ref='cog2id')
 
@@ -291,26 +303,25 @@ if __name__ == '__main__':
 
         d1 = phy.tree.get_distance(wl.tree, distance='rf')
         d2 = phy.tree.get_distance(t2, distance='rf')
-        d3 = phy.tree.get_distance(lingpy.basic.tree.Tree(lingpy.basic.tree.random_tree(taxa)),
-                distance='rf')
+        d3 = phy.tree.get_distance(
+            lingpy.basic.tree.Tree(lingpy.basic.tree.random_tree(taxa)),
+            distance='rf')
         dists1 += [d1]
         dists2 += [d2]
         dists3 += [d3]
 
-
         adist = sum([sum(x) for x in wl.distances]) / (len(wl.distances) ** 2)
         wlen = len(phy.words)
-        print('[i] generated tree {0} with distance of {1:.2f} vs. {2:.2f} vs.  {4:.2f} ({3:.2f}, {5}, {6:.2f}).'.format(
-                    i+1,
-                    d1,
-                    d2,
-                    adist,
-                    d3,
-                    wlen,
-                    wl.diversity))
+        print(
+            "[i] generated tree {0} with distance of "
+            "{1:.2f} vs. {2:.2f} vs. {4:.2f} ({3:.2f}, {5}, {6:.2f}).".format(
+                i+1,
+                d1,
+                d2,
+                adist,
+                d3,
+                wlen,
+                wl.diversity))
     print('Neighbor: {0:.2f}'.format(sum(dists1) / len(dists1)))
     print('UGPMA:    {0:.2f}'.format(sum(dists2) / len(dists2)))
     print('UGPMA:    {0:.2f}'.format(sum(dists3) / len(dists3)))
-
-
-

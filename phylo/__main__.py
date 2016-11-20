a04c4f736c2c1d8845edbe5532cce732b5adef81
Gereon Kaiping <anaphory@yahoo.de>
2016-11-20 21:15:36 +0100

    Pass change range from CLI through to simulation
    In the process, document what the `--min` and `--max` arguments to, and
    in that process also add help= to all other arguments.

--- phylo/__main__.py
+++ phylo/__main__.py
@@ -7,13 +7,20 @@ from .cli import run
 parser = argparse.ArgumentParser(description=""" Run a very simple forward-time
 phylogenetic simulation of cognate class evolution in a language
 family.""")
-parser.add_argument('-t', type=int, default=100)
-parser.add_argument('--max', type=int, default=2000)
-parser.add_argument('--min', type=int, default=1900)
-parser.add_argument('-s', type=int, default=1000)
-parser.add_argument('-f', type=int, default=50)
-parser.add_argument("-l", type=list, default="ABCDEFGHIJKLMN")
-parser.add_argument('-b', type=int, default=200)
+parser.add_argument('-t', type=int, default=100,
+                    help="Number of simulations to run with the same concept graph")
+parser.add_argument('--max', type=int, default=11000,
+                    help="Minimum number of change events along a branch")
+parser.add_argument('--min', type=int, default=9000,
+                    help="Maximum number of change events along a branch")
+parser.add_argument('-s', type=int, default=2000,
+                    help="Number of concepts to be simulated")
+parser.add_argument('-f', type=int, default=50,
+                    help="Number of semantic fields the concepts show")
+parser.add_argument("-l", type=list, default="ABCDEFGHIJKLMN",
+                    help="Taxon names")
+parser.add_argument('-b', type=int, default=200,
+                    help="Basic vocabulary size to be sampled")
 
 
 args = parser.parse_args()

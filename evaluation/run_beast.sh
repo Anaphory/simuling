3338d719dcfb2f2ab2cbe5256c173075f5c389fd
Gereon Kaiping <anaphory@yahoo.de>
2016-11-26 20:45:24 +0100

    Add files to run a BEAST analysis of the generated word lists

---
+++ evaluation/run_beast.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+while [ "!" -z $1 ]
+do
+    echo $1
+    root=`basename $1 .tsv`
+    mkdir $root
+    (
+        echo "ID	Language_ID	Feature_ID	Form	Weight	Global_CogID	Value";
+        tail -n +2 $1
+    ) | sed -e 's/	/,/g'| beastling beastling.ini -o $root/$root.xml
+    cd $root
+    beast $root.xml
+    shift
+done

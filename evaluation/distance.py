e24bda872cbe0e1943d19337e25ef8db3d9172af
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2017-01-31 12:35:33 +0100

    Add other dendropy tree distances

--- evaluation/distance.py
+++ evaluation/distance.py
@@ -6,6 +6,18 @@ import dendropy
 
 from dendropy.calculate import treecompare
 
+distance_functions = {
+    "euclidean": treecompare.euclidean_distance,
+    "bipartition": lambda t1, t2: sum(
+        treecompare.false_positives_and_negatives(t1, t2)),
+    "wrf": treecompare.weighted_robinson_foulds_distance,
+    "weighted_robinson_foulds": treecompare.weighted_robinson_foulds_distance,
+    "rf": treecompare.unweighted_robinson_foulds_distance,
+    "unweighted_robinson_foulds":
+        treecompare.unweighted_robinson_foulds_distance,
+}
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -21,6 +33,13 @@ if __name__ == "__main__":
         type=argparse.FileType('w'),
         default=sys.stdout,
         help="Output filename, one tree per input file")
+    parser.add_argument(
+        "--distance",
+        choices=distance_functions.keys(),
+        default="rf",
+        help="The function to use to calculate the distance between trees. "
+        "One of " + ", ".join(distance_functions.keys()))
+        
     args = parser.parse_args()
 
     for t1, t2 in zip(args.original, args.reconstructed):
@@ -29,4 +48,4 @@ if __name__ == "__main__":
             t1, schema="newick", taxon_namespace=tns)
         tree2 = dendropy.Tree.get_from_string(
             t2, schema="newick", taxon_namespace=tns)
-        print(treecompare.euclidean_distance(tree1, tree2))
+        print(distance_functions[args.distance](tree1, tree2))

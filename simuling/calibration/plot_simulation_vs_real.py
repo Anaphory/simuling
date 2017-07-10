c298dd3d30fbd0385746d612f5a5402cb7022c08
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2017-07-10 14:40:15 +0200

    Add more relevant output

--- simuling/calibration/plot_simulation_vs_real.py
+++ simuling/calibration/plot_simulation_vs_real.py
@@ -11,12 +11,13 @@ import bisect
 
 import matplotlib.pyplot as plt
 
+import os
 import sys
 import argparse
 
-import compare_simulation_with_data
-from compare_simulation_with_data import pairwise_shared_vocabulary
-from compare_simulation_with_data import read_cldf
+from . import compare_simulation_with_data
+from .compare_simulation_with_data import (
+    read_cldf, pairwise_shared_vocabulary)
 
 
 def ordered_pairwise_shared_vocabulary(data):
@@ -86,31 +87,52 @@ def main(args=sys.argv):
     parser.add_argument(
         "--figure-file",
         help="File to write the figure to")
+    parser.add_argument(
+        "--error-figure-file",
+        help="File to write the error plot to")
     args = parser.parse_args(args)
 
     x, names = ordered_pairwise_shared_vocabulary(read_cldf(args.realdata))
-    print("point", *["'{:}-{:}'".format(n1, n2) for n1, n2 in names],
+    print("point", "error", *["'{:}-{:}'".format(n1, n2) for n1, n2 in names],
           sep="\t")
-    print("real", *x, sep="\t")
+    print("real", "0", *x, sep="\t")
     plt.plot(x, x, "--", c="0.5")
 
     ax = plt.gca()
     ax.set_xticks(x)
     ax.set_xticklabels(names)
 
+    parameters = []
+    errors = []
     for sim in args.simulationdata:
         y = plot_vocabulary(x, names, read_cldf(sim))
-        print(sim.name, *y, sep="\t")
-
-    if args.figure_file:
-        plt.savefig(args.figure_file)
-    plt.show()
+        error = (sum([(xi-yi)**2 for xi, yi in zip(x, y)])/len(x))**0.5
+        try:
+            p = float(os.path.basename(sim.name).split("_")[1])
+        except (AttributeError, TypeError):
+            p = float("nan")
+        parameters.append(p)
+        errors.append(error)
+        print(sim.name, p, error, *y, sep="\t")
 
     for tick in ax.xaxis.get_major_ticks():
         tick.label.set_fontsize(6)
         # specify integer or one of preset strings, e.g.
         # tick.label.set_fontsize('x-small')
         tick.label.set_rotation('vertical')
+
+    if args.figure_file:
+        plt.savefig(args.figure_file)
+    else:
+        plt.show()
+
+    plt.figure()
+
+    plt.scatter(parameters, errors)
+    if args.figure_file:
+        plt.savefig(args.error_figure_file)
+    else:
+        plt.show()
 
 
 if __name__ == "__main__":
